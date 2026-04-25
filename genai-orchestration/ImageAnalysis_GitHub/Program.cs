6f26f5a90d8aeb601a1e4cbd6995e09969a0dbc6
astro5_L <joyultimates@gmail.com>
2026-04-25 13:14:38 +0600

    reorganize the codebase

--- genai-orchestration/ImageAnalysis_GitHub/Program.cs
+++ genai-orchestration/ImageAnalysis_GitHub/Program.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Extensions.AI;
+﻿using ImageAnalysis_GitHub;
+using Microsoft.Extensions.AI;
 using Microsoft.Extensions.Configuration;
 using OpenAI;
 using System.ClientModel;
@@ -19,50 +20,8 @@ IChatClient client =
     new OpenAIClient(credential, options).GetChatClient(gitHubModel).AsIChatClient();
 
 
-// user prompts
-var promptDescribe = "Describe the image";
-var promptAnalyze = "How many red cars are in the picture? and what other car colors are there?";
-
-// prompts
-string systemPrompt = "You are a useful assistant that describes images using a direct style.";
-var userPrompt = promptAnalyze;
-
-List<ChatMessage> messages =
-[
-    new ChatMessage(ChatRole.System, systemPrompt),
-    new ChatMessage(ChatRole.User, userPrompt),
-];
-
-// read the image bytes, create a new image content part and add it to the messages
-// Define your file path
-var imageFileName = "traffic.jpg"; // Or "traffic.jpg"
-string imagePath = Path.Combine(Directory.GetCurrentDirectory(), "images", imageFileName);
-
-// Detect the MIME type dynamically
-string mimeType = GetMimeType(imageFileName);
-
-// Create the DataContent using the detected type
-AIContent aic = new DataContent(File.ReadAllBytes(imagePath), mimeType);
-
-var message = new ChatMessage(ChatRole.User, [aic]);
-messages.Add(message);
+// analyze the imagese
+await ImageAnalysis.AnalyzeImage(client);
 
 
-// send the messages to the assistant
-var response = await client.GetResponseAsync(messages);
-Console.WriteLine($"Prompt: {userPrompt}");
-Console.WriteLine($"Image: {imageFileName}");
-Console.WriteLine($"Response: {response.Messages[0]}");
 
-static string GetMimeType(string fileName)
-{
-    string extension = Path.GetExtension(fileName).ToLowerInvariant();
-    return extension switch
-    {
-        ".png" => "image/png",
-        ".jpg" or ".jpeg" => "image/jpeg",
-        ".gif" => "image/gif",
-        ".webp" => "image/webp",
-        _ => "application/octet-stream" // Fallback for unknown types
-    };
-}
